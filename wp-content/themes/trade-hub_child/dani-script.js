99e97e69cd3d9ded30fee9444425ddceddbfa244
djorde91 <djorde91@gmail.com>
2019-06-13 16:50:03 +0200

    custom scripts

---
+++ wp-content/themes/trade-hub_child/dani-script.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function($){
+
+console.log('daniel jorde script loaded');
+
+  setTimeout(function(){
+//jQuery("#gd_calendar .gd_calendar_year_table .gd_calendar_header_year:nth-child(1)").css('background' , 'red');
+//jQuery("#gd_calendar .gd_calendar_year_table .gd_calendar_header_year:nth-child(1)").insertAfter( jQuery( "#gd_calendar .gd_calendar_year_table .gd_calendar_header_year:nth-child(7)" ).this );
+
+
+  }, 1500);
+});
